329783f997401a1d42477752b191b7f12dd2d13c
__HxK__ <46660187+GabrielCotua@users.noreply.github.com>
2025-02-25 16:35:45 -0600

    .

--- HW06.c
+++ HW06.c
@@ -7,41 +7,17 @@ int valueTarget = 0;
 long long factoredNumber = 1;
 int i = 0;
 
-int isValueValid(void) // Evaluate if the input is valid
-{
-    do
-    {
-        printf("Insert a number between 1 and 20 to get the factorial of: ");
-        if (scanf("%lld", &valueTarget) == 1)
-        { // If input is not a valid integer
-            if (valueTarget < 0 || valueTarget > 20) 
-            {
-                return 1;
-            }
-
-        }
-        
-    } while ( (valueTarget < 0 || valueTarget > 20) );
-    return 0;
-}
-
-int isValueValid_1(void) 
+int isValueValid(void) 
 {
     printf("Insert a number between 1 and 20 to get the factorial of: ");
-    while(scanf("%d", &valueTarget) != 1)
+    scanf("%d", &valueTarget);
+
+    while( ( valueTarget < 0 || valueTarget > 20 ) )
     {
-        
-        if (valueTarget == 'q' || valueTarget == 0 ) 
-        {
-            printf("Byebye");
-            return 0;
-        }
-        if (valueTarget < 0 || valueTarget > 20)
-        {
-            return 1;
-        }
         printf("Insert a number between 1 and 20 to get the factorial of: ");
+        scanf("%d", &valueTarget);
     }
+    return 1;
 }
 
 void getFactorial(void) // Returns factorial result
@@ -57,7 +33,7 @@ int main(void) // Runner function
 
     printf("\n\n|#####################|\n|HW #06, Gabriel Cotua|\n|#####################|\n\n");
 
-    if ( isValueValid_1() )
+    if ( isValueValid() )
     {
         getFactorial();
         printf("The factor of your number is: %lld", factoredNumber);
